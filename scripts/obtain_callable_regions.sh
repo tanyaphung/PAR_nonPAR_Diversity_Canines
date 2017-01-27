d526ee654576f267432113e059fe5c9a4bf2af04
tnphung <tnphung@ucla.edu>
2017-01-27 15:37:19 -0800

    Remove the help function. Will need to incorporate that back later

--- scripts/obtain_callable_regions.sh
+++ scripts/obtain_callable_regions.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-
-while [[ $# -gt 0 ]]
-do
-opt="$1"
-
-case $opt in
-  -h|--help)
-  echo "This script is used to obtain the callable regions from a bam file using GATK. To run this, inputs are 1. Path to GATK, 2. Path to the reference genome, 3. Path to the output directory, 4. Path to the bam file, 5. Name of the individual. Note that input should be in this order."
-  shift
-  ;;
-  *)
-  ;;
-esac
-done
-
 . /u/local/Modules/default/init/modules.sh
 module load java/1.8.0_77
 MEM=4G
@@ -31,4 +16,4 @@ java -Xmx$MEM -jar ${GATK} \
 	--minDepth 10 \
 	--minMappingQuality 30 \
 	-summary ${outDir}/${sampleabb}/${sampleabb}'_table.txt' \
-	-o ${outDir}/${sampleabb}/${sampleabb}'_callable_loci.bed'
+	-o ${outDir}/${sampleabb}/${sampleabb}'_callable_loci.bed
