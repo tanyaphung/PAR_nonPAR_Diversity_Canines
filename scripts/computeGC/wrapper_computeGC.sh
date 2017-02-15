65a36f7ba76865163641be966cdccc44a6cdc786
tnphung <tnphung@ucla.edu>
2017-02-15 14:08:27 -0800

    working version of scripts used to comptue GC content

--- scripts/computeGC/wrapper_computeGC.sh
+++ scripts/computeGC/wrapper_computeGC.sh
@@ -7,12 +7,12 @@
 #$ -m ea
 
 #chrX
-#chrom=chrX
-#windows='/u/project/klohmuel/tanya_data/Canines_Project_Data/canFam3.1_50kb_nonoverlapping_windows/'${chrom}'_50kb_windows.bed'
-#targets='/u/project/klohmuel/tanya_data/PAR_Cannines_project/analyses/ensembleGenes.repeatMasker.microsat.simpleRepeats_concat/'${chrom}'/4.sort.merge.complement_'${chrom}'_ensembleGenes.repeatMasker.microsat.simpleRepeats.bed'
-#out='/u/home/p/phung428/nobackup-kirk/tanya_data/R_GC/'${chrom}'_GC.txt'
+chrom=chrX
+windows='/u/project/klohmuel/tanya_data/Canines_Project_Data/canFam3.1_50kb_nonoverlapping_windows/'${chrom}'_50kb_windows.bed'
+targets='/u/project/klohmuel/tanya_data/PAR_Cannines_project/analyses/ensembleGenes.repeatMasker.microsat.simpleRepeats_concat/'${chrom}'/4.sort.merge.complement_'${chrom}'_ensembleGenes.repeatMasker.microsat.simpleRepeats.bed'
+out='/u/home/p/phung428/nobackup-kirk/tanya_data/R_GC/'${chrom}'_GC.txt'
 
-#python compute_GC.py --windows_bed ${windows} --targets_bed ${targets} --chr ${chrom} --outfile ${out}
+python compute_GC.py --windows_bed ${windows} --targets_bed ${targets} --chr ${chrom} --outfile ${out}
 
 # For autosomes
 for i in {01..38}
