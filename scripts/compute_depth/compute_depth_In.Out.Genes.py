3cf9fbfc1a2cf490d18a9ee918a8c0406a95f893
tnphung <tnphung@ucla.edu>
2017-04-20 15:09:53 -0700

    Speed up script

--- scripts/compute_depth/compute_depth_In.Out.Genes.py
+++ scripts/compute_depth/compute_depth_In.Out.Genes.py
@@ -3,6 +3,7 @@ import os
 import argparse
 import csv
 import numpy
+from bisect import bisect
 
 def parse_args():
 	"""
@@ -25,14 +26,14 @@ def main():
 
 	result_file = open(args.outfile, "w")
 
-	depth_set = set()
+	# depth_set = set()
 	positions = []
 	depths = []
 	with open(args.depth, "r") as DEPTH_file:
 		for line in DEPTH_file:
 			line = line.rstrip("\n")
 			line = line.split("\t")
-			depth_set.add(int(line[1]))
+			# depth_set.add(int(line[1]))
 			positions.append(int(line[1]))
 			depths.append(int(line[2]))
 
@@ -40,11 +41,26 @@ def main():
 		for line in BED_file:
 			line = line.rstrip("\n")
 			line = line.split("\t")
-			total_depth = []
-			for each_site in range(int(line[1]), int(line[2])):
-				if each_site in depth_set:
-					each_site_index = positions.index(each_site)
-					total_depth.append(depths[each_site_index])
+			start = int(line[1])
+			end = int(line[2])
+
+			# find start:
+			if start in positions:
+				start_index = positions.index(start)
+			else:
+				if start <= positions[0]:
+					start_index = 0
+				else:
+					start_index = bisect(positions, start)
+
+			# find end:
+			if end in positions:
+				end_index = positions.index(end)
+			else:
+				end_index = bisect(positions, end) - 1
+
+
+			total_depth = depths[start_index:end_index]
 			mean_depth = numpy.mean(total_depth)
 
 			output = [str(line[0]), str(line[1]), str(line[2]), str(mean_depth)]
