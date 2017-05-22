e575768c89ebd06ae3cdd8cf034b50da74d3fb0e
tnphung <tnphung@ucla.edu>
2017-05-22 08:42:12 -0700

    Add scripts for leave one breed out analyses

--- scripts/compute_diversity_leave_one_out/wrapper_scripts/wrapper_computepairwiseDiversity_13Dogs_leave_out_corgi.sh
+++ scripts/compute_diversity_leave_one_out/wrapper_scripts/wrapper_computepairwiseDiversity_13Dogs_leave_out_corgi.sh
@@ -5,9 +5,9 @@ chrNum=X
 
 windows=../../../results/make_nonoverlapping_windows/50kb/chr${chrNum}_50kb_windows.bed
 targets=../../../results/get_target/obtain_callable_in_neutral_for_diversity/0kb_flanking/13Dogs/13Dogs_chr${chrNum}_callableSites_in_neutralRegions.txt
-variants=../../../results/compute_diversity_leave_one_out/genotype_files_for_calc_diversity/chr${chrNum}/chr${chrNum}_leave_out_airedale.GT.FORMAT
-numAllele=24
-out=../../../results/compute_diversity_leave_one_out/13Dogs/leave_out_airedale/chr${chrNum}_13Dogs_diversity_leave_out_airedale.txt
+variants=../../../results/compute_diversity_leave_one_out/genotype_files_for_calc_diversity/chr${chrNum}/chr${chrNum}_leave_out_corgi.GT.FORMAT
+numAllele=22
+out=../../../results/compute_diversity_leave_one_out/13Dogs/leave_out_corgi/chr${chrNum}_13Dogs_diversity_leave_out_corgi.txt
 
 python ../compute_pairwise_diversity.py --windows_bed ${windows} --targets_bed ${targets} --variants ${variants} --numAllele ${numAllele} --outfile ${out}
 
@@ -16,9 +16,9 @@ do
 
 windows=../../../results/make_nonoverlapping_windows/50kb/chr${chrNum}_50kb_windows.bed
 targets=../../../results/get_target/obtain_callable_in_neutral_for_diversity/0kb_flanking/13Dogs/13Dogs_chr${chrNum}_callableSites_in_neutralRegions.txt
-variants=../../../results/compute_diversity_leave_one_out/genotype_files_for_calc_diversity/chr${chrNum}/chr${chrNum}_leave_out_airedale.GT.FORMAT
-numAllele=24
-out=../../../results/compute_diversity_leave_one_out/13Dogs/leave_out_airedale/chr${chrNum}_13Dogs_diversity_leave_out_airedale.txt
+variants=../../../results/compute_diversity_leave_one_out/genotype_files_for_calc_diversity/chr${chrNum}/chr${chrNum}_leave_out_corgi.GT.FORMAT
+numAllele=22
+out=../../../results/compute_diversity_leave_one_out/13Dogs/leave_out_corgi/chr${chrNum}_13Dogs_diversity_leave_out_corgi.txt
 
 python ../compute_pairwise_diversity.py --windows_bed ${windows} --targets_bed ${targets} --variants ${variants} --numAllele ${numAllele} --outfile ${out}
 
